3596a0dc4a1a6af3ddf12f8357444cc725a7250f
Zhanyiwaa <5js2vhymsb@privaterelay.appleid.com>
2026-02-04 11:06:34 +0500

    examples

---
+++ Practice2/boolean/boolean_operators.py
@@ -0,0 +1,14 @@
+'''1.Print "YES!" if the function returns True, otherwise print "NO!":
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")'''
+
+'''2.Check if an object is an integer or not:
+x = 200
+print(isinstance(x, int))
+
+'''
